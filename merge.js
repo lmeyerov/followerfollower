29e29c2d815a2f0b16fea19692a6b4db07563c80
Leo Meyerovich <leo@graphistry.com>
2015-03-25 15:23:48 -0700

    trimming excess data & followers as part of merge

--- merge.js
+++ merge.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --max-old-space-size=8192
+#!/usr/bin/env node --max-old-space-size=12192
 // node merge.js out.json in1.json in2.json ...
 
 'use strict';
@@ -40,6 +40,62 @@ data.forEach(function (dict) {
         }
     }
 });
+
+
+debug('counting nodes');
+var degrees = {};
+for (var name in merged) {
+    var act = merged[name];
+    if (act.nfo) {
+        degrees[act.nfo.id] = 2;
+    }
+    if (act.followers) {
+        for (var i = 0; i < act.followers.length; i++) {
+            var id = act.followers[i];
+            degrees[id] = (degrees[id] || 0) + 1;
+        }
+    }
+}
+debug('counted accts', _.keys(merged).length);
+var highDegree = 0;
+for (var i in degrees) {
+    if (degrees[i] > 1) {
+        highDegree++;
+    }
+}
+debug('counted ids', _.keys(degrees).length, 'used', highDegree);
+
+debug('trimming supernodes')
+for (var name in merged) {
+    var act = merged[name];
+    var SAMPLE = 100000000;
+    if (act.followers && act.followers.length > SAMPLE * 10) {
+        var ok = [];
+        for (var i = 0; i < SAMPLE; i++) {
+            ok.push(act.followers[i]);
+        }
+        for (var i = SAMPLE; i < act.followers.length; i++) {
+            var id = act.followers[i];
+            if (degrees[id] > 1) {
+                ok.push(i);
+            }
+        }
+        act.followers = ok;
+    }
+    act.nfo =
+        act.nfo ?
+            _.object(
+            ['created_at', 'description', 'distance',
+             'favourites_count', 'followers_count', 'friends_count',
+             'geo_enabled', 'id', 'name', 'screen_name',
+             'statuses_count', 'url', 'verified',
+             'profile_image_url', 'profile_image_url_https']
+            .map(function (fld) {
+                return [fld, act.nfo[fld]]
+            }))
+            : undefined;
+}
+
 
 debug('loaded');
 
